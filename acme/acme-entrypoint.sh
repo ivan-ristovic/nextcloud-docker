3e68d3b7b41a702c5b3987f4687dc78399d32b1c
ivan-ristovic <ivan.ristovic95@gmail.com>
2021-12-30 19:32:44 +0100

    Upload current version

---
+++ acme/acme-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+for DOMAIN in $DOMAINS; do
+  if [ ! -f "/acme.sh/${DOMAIN}/${DOMAIN}.cer" ]; then
+    /usr/local/bin/--issue \
+      -d "${DOMAIN}" \
+      -w "/acme-challenge/${DOMAIN}" \
+        || { echo "Could not issue certificate for ${DOMAIN}"; exit 1; }
+  fi
+
+  2>/dev/null mkdir "/ssl/${DOMAIN}"
+  touch "/ssl/${DOMAIN}"/{fullchain.pem,cert.pem,key.pem}
+  chmod 0600 "/ssl/${DOMAIN}/key.pem"
+  chown -R ${INSTALL_UID}:${INSTALL_GID} "/ssl/${DOMAIN}"
+
+  /usr/local/bin/--install-cert \
+    -d "${DOMAIN}" \
+    --cert-file "/ssl/${DOMAIN}/cert.pem" \
+    --key-file "/ssl/${DOMAIN}/key.pem" \
+    --fullchain-file "/ssl/${DOMAIN}/fullchain.pem" \
+      || { echo "Could not install certificate for ${DOMAIN}"; exit 1; }
+done
+
+exec /entry.sh daemon
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xe;
+
+source .env
+
+echo 'Creating nececary directories in $FS_ROOT'
+mkdir -p "${FS_ROOT}"/{acme-challenge,acme.sh,ssl,postgres,nextcloud}
+
+echo 'Switching default CA to LetsEncrypt'
+docker run --rm -v "${FS_ROOT}/acme.sh:/acme.sh" neilpang/acme.sh \
+	acme.sh --set-default-ca --server letsencrypt
+
+echo 'Registering account with CA: $ACME_EMAIL'
+docker run --rm -v "${FS_ROOT}/acme.sh:/acme.sh" neilpang/acme.sh \
+	acme.sh --register-account -m "${ACME_EMAIL}"
+
+echo 'Done! You may now run `docker-compose up -d` with root privileges to start the service stack'
