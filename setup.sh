88ee1961f66ed13e6e05372f08074f6b2e8c726a
ivan-ristovic <ivan.ristovic95@gmail.com>
2022-01-01 13:09:35 +0100

    Set up nextcloud cron

--- setup.sh
+++ setup.sh
@@ -15,4 +15,7 @@ echo 'Registering account with CA: $ACME_EMAIL'
 docker run --rm -v "${DATA_ROOT}/acme.sh:/acme.sh" neilpang/acme.sh \
 	acme.sh --register-account -m "${ACME_EMAIL}"
 
+echo 'Building the service stack...'
+docker-compose build
+
 echo 'Done! You may now run `docker-compose up -d` with root privileges to start the service stack'
